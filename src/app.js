a6a82927612b437b619bd8a40de750709245408e
unknown <sronitha.lakshmi@gmail.com>
2022-04-05 09:14:32 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const path=require('path');
 const express=require('express');
 const app=express();
 const {accounts,users,writeJSON} = require('./data');
-const accountRoutes = require('../routes/accounts');
-const servicesRoutes = require('../routes/services');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 const app = express();
 
 
