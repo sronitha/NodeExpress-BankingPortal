67c12002bed4e8178209d1ffc9364d8d1c951867
unknown <sronitha.lakshmi@gmail.com>
2022-04-06 06:39:09 +0100

    fixed

--- src/app.js
+++ src/app.js
@@ -1,28 +1,35 @@
-const fs= require('fs');
-const path=require('path');
-const express=require('express');
-const app=express();
-const {accounts,users,writeJSON} = require('./data');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
 const accountRoutes = require('./routes/accounts');
 const servicesRoutes = require('./routes/services');
+
 const app = express();
 
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.urlencoded({extended:true}));
+const { accounts, users, writeJSON } = require('./data');
 
-
-
-app.get('/',(req,res)=>res.render('index',{title:'Account Summary',accounts:accounts}));
-app.use('/account',accountRoutes);
-app.use('/services',servicesRoutes);
-
-app.get('/profile',(req,res)=>res.render('profile',{user : users[0]}));
+const port = process.env.PORT || 3000;
 
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Account Summary', accounts});
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0]})
+})
 
 
-
-app.listen(3000,() => console.log('PS Project running on port 3000!'))
+app.listen(port, () => {
+    console.log('PS Project Running on Port 3000!')
+})
